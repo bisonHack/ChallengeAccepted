7f789bcc5e815893241a86ea33c7ecb622dc2399
popoolasubomi <popoolaogooluwasubomi@gmail.com>
2021-03-19 15:33:34 -0400

    Installed Pasrse SDK

--- ChallengeAccepted/ChallengeAccepted/AppDelegate.swift
+++ ChallengeAccepted/ChallengeAccepted/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -13,7 +14,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        let parseConfig = ParseClientConfiguration {
+              $0.applicationId = "7megvzwBN2WkLxQxBtezhjNSkSzpcS6RoOo3hMYJ"
+              $0.clientKey = "6hHo5nBVWYfCmQ8QqRaQesOfRUAQGetxmSqHK5ex"
+              $0.server = "https://parseapi.back4app.com"
+          }
+          Parse.initialize(with: parseConfig)
         return true
     }
 
