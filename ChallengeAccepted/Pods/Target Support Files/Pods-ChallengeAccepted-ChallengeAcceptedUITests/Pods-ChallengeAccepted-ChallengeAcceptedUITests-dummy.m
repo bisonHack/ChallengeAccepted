e955998425ec735cfebcc95f1fbf0ee5fcb5d718
popoolasubomi <popoolaogooluwasubomi@gmail.com>
2021-03-19 14:26:54 -0400

    Installed Parse Pods

---
+++ ChallengeAccepted/Pods/Target Support Files/Pods-ChallengeAccepted-ChallengeAcceptedUITests/Pods-ChallengeAccepted-ChallengeAcceptedUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ChallengeAccepted_ChallengeAcceptedUITests : NSObject
+@end
+@implementation PodsDummy_Pods_ChallengeAccepted_ChallengeAcceptedUITests
+@end
