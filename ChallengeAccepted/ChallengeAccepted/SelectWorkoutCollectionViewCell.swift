1aa07ef51876c27fdf005b563d8e36b633b37baa
George Martin <georgeg10live@gmail.com>
2021-04-13 21:13:30 -0400

    added code for collection view and collection view cell

---
+++ ChallengeAccepted/ChallengeAccepted/SelectWorkoutCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  SelectWorkoutCollectionViewCell.swift
+//  ChallengeAccepted
+//
+//  Created by George Martin on 4/13/21.
+//
+
+import UIKit
+
+class SelectWorkoutCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
--- ChallengeAccepted/ChallengeAccepted/SelectWorkoutViewController.swift
+++ ChallengeAccepted/ChallengeAccepted/SelectWorkoutViewController.swift
@@ -9,13 +9,38 @@ import UIKit
 
 class SelectWorkoutViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
 
+    var workouts = [[String:Any]]()
+    
+    @IBOutlet weak var collectionview: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        collectionview.delegate = self
+        collectionview.delegate = self
+        
+        let layout = collectionview.collectionViewLayout as! UICollectionViewFlowLayout
+        
+        layout.minimumLineSpacing = 4
+        layout.minimumInteritemSpacing = 4
+        
+        let width = (view.frame.size.width - layout.minimumInteritemSpacing * 2) / 3
+        layout.itemSize = CGSize(width: width, height: width * 3 / 2)
         // Do any additional setup after loading the view.
     }
     
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
 
+        return workouts.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MovieGridCollectionViewCell", for: indexPath) as! MovieGridCollectionViewCell
+        
+        let workout = workouts[indexPath.item]
+        //cell.posterView.af_setImage(withURL: posterURL!)
+        
+        return cell
+    }
     /*
     // MARK: - Navigation
 
